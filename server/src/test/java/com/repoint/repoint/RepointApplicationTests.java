9a9bd6df04496f809628f7dfab8e9053f85ef700
오세영 <leenight5309@naver.com>
2026-07-28 20:02:01 +0900

    feat: 환경 설정

---
+++ server/src/test/java/com/repoint/repoint/RepointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repoint.repoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
